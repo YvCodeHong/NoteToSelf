86b953db465c72c6ec2183f36b0e1f35496ac0c7
Yvonne Tang <ytang604@gmail.com>
2018-08-12 17:36:12 +0100

    add create react app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+//it is the foundational library
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
